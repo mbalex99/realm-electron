6870e60310d7a77f3ce1e0697318b5fcac270a2c
Max Alexander <mbalex99@gmail.com>
2017-03-13 23:15:11 -0700

    adding react

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div>
+                <h1>Hey</h1>
+            </div>
+        )
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader';
+
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('app'))
